0eb90d0e5bfd84c3722de71c09a0daab1d74ab4b
Anderson Assum <anderson.assum12@gmail.com>
2023-08-15 17:24:39 -0300

    first commit

---
+++ script2.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+        document.querySelector('.box-load').style.display = "none";
+        document.querySelector('.content').style.display = "block";
+    }, 13000);
+});
